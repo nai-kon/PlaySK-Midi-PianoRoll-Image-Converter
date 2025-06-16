2a8d13c396d16ccb6c532edca6c66e1b4b78d4f0
nai-kon <fxtch686@yahoo.co.jp>
2025-06-16 20:43:22 +0900

    refactoring

--- src/Midi2Image.java
+++ src/Midi2Image.java
@@ -15,65 +15,64 @@ import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 public class Midi2Image {
-
-	final static int ROLL_DPI = 300;
-	final static int ROLL_TEMPO = 0;
-	final static int ROLL_WIDTH_PX = (int) (11.25 * ROLL_DPI);
-	final static int ROLL_MARGIN_PX = 50; // @300dpi
-	final static int HOLE_WIDTH_PX = 18; // @300dpi
-	final static int NOTE0_CENTER_INCH = 36; // @300dpi
-	final static int NOTE99_CENTER_INCH = 3340; // @300dpi
-	final static int ROLL_COLOR = 120; // gray scale
-	final static int SUSTAIN_NOTE_NO = 18;
-	final static int SOFTPEDAL_NOTE_NO = 113;
-
-	protected static int getHoleX(int note_no) {
+           
+	private int rollDpi = 300;                                                                 
+	private int rollTempo = 95;                                                                
+	private int rollWidth = (int) (11.25 * this.rollDpi);  // @300dpi                          
+	private int rollMargin = 50; // @300dpi                                                    
+	private int holeWidth = 18; // @300dpi                                                     
+	private int hole0Center = 36; // @300dpi                                                   
+	private int hole99Center = 3340; // @300dpi                                                
+	private int rollColor = 120; // gray scale              
+	private int reducePX = 10;                                                       
+	int[][] controlChange2NoteNo = {{64, 18}, {67, 113}};  // Control Change No to Hole No. Sustain/Soft pedals.              
+                                                                                               
+	private BufferedImage out_img = null;
+	private Graphics2D g2d = null;
+	       
+	Midi2Image(){                    
+	}
+	
+	protected int getHoleX(int note_no) {
 		// Return hole x position in pixel by note number 
-		return ROLL_MARGIN_PX + NOTE0_CENTER_INCH + ((note_no - 15) * (NOTE99_CENTER_INCH - NOTE0_CENTER_INCH) / 99) - (HOLE_WIDTH_PX / 2);
+		return rollMargin + hole0Center + ((note_no - 15) * (hole99Center - hole0Center) / 99) - (holeWidth / 2);
 	}
 
-	protected static int getTick2Px(long tickLen, int tempo, double bpm, int ppq) {
+	protected int getTick2Px(long tickLen, int tempo, double bpm, int ppq) {
 		// Return hole length in pixel by MIDI tick length
-		return (int) ((tickLen * ROLL_DPI * tempo * 1.2) / (bpm * ppq));
+		return (int) ((tickLen * rollDpi * tempo * 1.2) / (bpm * ppq));
 	}
 
-	protected static void drawHole(Graphics2D g2d, int img_length, int note, int tempo, double bpm, int ppq,
-			long onTick, long offTick) {
-		int hole_h = getTick2Px(offTick - onTick, tempo, bpm, ppq);
+	protected void drawHole(int note, int tempo, double bpm, int ppq, long onTick, long offTick) {
+		int hole_h = getTick2Px(offTick - onTick, tempo, bpm, ppq) - reducePX;
+		hole_h = hole_h < holeWidth ? holeWidth : hole_h; 
 		int hole_x = getHoleX(note);
-		int hole_y = img_length - getTick2Px(onTick, tempo, bpm, ppq) - hole_h;
-		int hole_y_margin = (int) HOLE_WIDTH_PX / 3;
+		int hole_y = out_img.getHeight() - getTick2Px(onTick, tempo, bpm, ppq) - hole_h;
+		int hole_y_margin = (int) holeWidth / 3;
 		int chain_perforation_th_len = 85;
 		g2d.setColor(Color.WHITE);
 		
 		// Chain Perforation
 		int y = hole_y;
-		for (; y < hole_y + hole_h - chain_perforation_th_len; y += hole_y_margin + HOLE_WIDTH_PX) {
-			g2d.fillOval(hole_x, y, HOLE_WIDTH_PX, HOLE_WIDTH_PX);
+		for (; y < hole_y + hole_h - chain_perforation_th_len; y += hole_y_margin + holeWidth) {
+			g2d.fillOval(hole_x, y, holeWidth, holeWidth);
 		}
 		// Normal Perforation
-		g2d.fillRoundRect(hole_x, y, HOLE_WIDTH_PX, hole_h - (y - hole_y), HOLE_WIDTH_PX, HOLE_WIDTH_PX);
+		g2d.fillRoundRect(hole_x, y, holeWidth, hole_h - (y - hole_y), holeWidth, holeWidth);
 		// somewhow, fillOval() result pops out 1px on left, so fill it.
-		g2d.setColor(new Color(ROLL_COLOR, ROLL_COLOR, ROLL_COLOR));
+		g2d.setColor(new Color(rollColor, rollColor, rollColor));
 		g2d.drawLine(hole_x, hole_y, hole_x, hole_y + hole_h);
 	}
-
-	public static void main(String[] args) {
-
-		long startTime = System.currentTimeMillis();
-		Sequence sequence;
+	
+	public void convert(String midiPath, String outDir) {
 		try {
-			var fname = "【オリジナル曲】自動演奏ピアノのためにⅢ.mid";
-			sequence = MidiSystem.getSequence(new File(fname));
+			Sequence sequence = MidiSystem.getSequence(new File(midiPath));
 			int ppq = sequence.getResolution(); // PPQ (ticks per quarter note)
 			long totalTickLen = sequence.getTickLength();
 
-			BufferedImage out_img = null;
-			Graphics2D g2d = null;
-			int img_h = 0;
 			double bpm = 80;
-			int tempo = ROLL_TEMPO;
-			System.out.println("Resolution (PPQ): " + ppq);
+			int tempo = rollTempo;
+			
 			for (Track track : sequence.getTracks()) {
 				long[] noteOnTicks = new long[128];
 				for (int i = 0; i < track.size(); i++) {
@@ -89,17 +88,17 @@ public class Midi2Image {
 							tempo = (int) Math.round(bpm);
 						System.out.printf("Tempo event at tick %d: %.2f BPM %d tempo\n", evt.getTick(), bpm, tempo);
 						// Create Image Background
-						int width = ROLL_WIDTH_PX + 2 * ROLL_MARGIN_PX;
-						img_h = getTick2Px(totalTickLen, tempo, bpm, ppq);
+						int width = rollWidth + 2 * rollMargin;
+						int img_h = getTick2Px(totalTickLen, tempo, bpm, ppq);
 						out_img = new BufferedImage(width, img_h, BufferedImage.TYPE_BYTE_GRAY);
 						g2d = out_img.createGraphics();
 						// Draw Roll Edge
 						g2d.setColor(Color.WHITE);
-						g2d.fillRect(0, 0, ROLL_MARGIN_PX, img_h);
-						g2d.fillRect(ROLL_WIDTH_PX + ROLL_MARGIN_PX, 0, ROLL_MARGIN_PX, img_h);
+						g2d.fillRect(0, 0, rollMargin, img_h);
+						g2d.fillRect(rollWidth + rollMargin, 0, rollMargin, img_h);
 						// Draw Roll
-						g2d.setColor(new Color(ROLL_COLOR, ROLL_COLOR, ROLL_COLOR));
-						g2d.fillRect(ROLL_MARGIN_PX, 0, ROLL_WIDTH_PX, img_h);
+						g2d.setColor(new Color(rollColor, rollColor, rollColor));
+						g2d.fillRect(rollMargin, 0, rollWidth, img_h);
 						g2d.setColor(Color.WHITE);
 					}
 
@@ -109,39 +108,44 @@ public class Midi2Image {
 						int velocity = sm.getData2();
 						long tick = evt.getTick();
 
-						// Sustain Pedal On/Off
-						if (cmd == ShortMessage.CONTROL_CHANGE && note == 64 && velocity > 0) {
-							noteOnTicks[SUSTAIN_NOTE_NO] = tick;
-						} else if (cmd == ShortMessage.CONTROL_CHANGE && note == 64 && velocity == 0) {
-							long onTick = noteOnTicks[SUSTAIN_NOTE_NO];
-							drawHole(g2d, img_h, SUSTAIN_NOTE_NO, tempo, bpm, ppq, onTick, tick);
+						// Pedal Control Change to Note No
+						for (int[] pair : controlChange2NoteNo) {
+						    int cc_number = pair[0];
+						    int note_numer = pair[1];
+							if (cmd == ShortMessage.CONTROL_CHANGE && note == cc_number && velocity > 0) {
+								noteOnTicks[note_numer] = tick;
+							} else if (cmd == ShortMessage.CONTROL_CHANGE && note == cc_number && velocity == 0) {
+								long onTick = noteOnTicks[note_numer];
+								drawHole(note_numer, tempo, bpm, ppq, onTick, tick);
+							}
 						}
 
-						// Soft Pedal On/Off
-						if (cmd == ShortMessage.CONTROL_CHANGE && note == 67 && velocity > 0) {
-							noteOnTicks[SOFTPEDAL_NOTE_NO] = tick;
-						} else if (cmd == ShortMessage.CONTROL_CHANGE && note == 67 && velocity == 0) {
-							long onTick = noteOnTicks[SOFTPEDAL_NOTE_NO];
-							drawHole(g2d, img_h, SOFTPEDAL_NOTE_NO, tempo, bpm, ppq, onTick, tick);
-						}
-						
+						// Note On/Off
 						if (cmd == ShortMessage.NOTE_ON && velocity > 0) {
 							noteOnTicks[note] = tick;
 						} else if (cmd == ShortMessage.NOTE_OFF || cmd == ShortMessage.NOTE_ON && velocity == 0) {
 							long onTick = noteOnTicks[note];
-							drawHole(g2d, img_h, note, tempo, bpm, ppq, onTick, tick);
+							drawHole(note, tempo, bpm, ppq, onTick, tick);
 						}
 					}
 				}
-			}
-
-			String save_name = fname.replace(".mid", String.format(" tempo%d.png", tempo));
+			}	
+			// save image
+			String save_name = outDir + (new File(midiPath)).getName().replace(".mid", String.format(" tempo%d.png", tempo));
 			ImageIO.write(out_img, "png", new File(save_name));
-			System.out.println("処理時間：" + (System.currentTimeMillis() - startTime) + " ms");
-
 		} catch (InvalidMidiDataException | IOException e) {
 			e.printStackTrace();
-		}
+		}	
+	}
 
+	public static void main(String[] args) {
+
+		File dir = new File("C:\\Users\\sasaki\\Downloads\\Ampico_All_erolls\\test\\");
+		for (File file : dir.listFiles()) {
+		    if (file.isDirectory()) continue;
+		    
+			var obj = new Midi2Image();
+			obj.convert(file.getPath(), "output/classic/");
+		}	    		
 	}
 }
