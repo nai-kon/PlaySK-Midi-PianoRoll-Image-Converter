b06f9ff62811028dd59cfad3e802d0cb9951d145
nai-kon <fxtch686@yahoo.co.jp>
2025-06-28 22:53:20 +0900

    WIP add GUI

--- src/main_ui.py
+++ src/main_ui.py
@@ -32,86 +32,114 @@ class MainFrame():
         self.parent = parent
         self.create_sidebar()
         self.roll_viewer: RollViewer | None = None
-        self.midi2image = Midi2Image()
 
     def convert(self):
-        self.midi2image.convert("Hits of the Day 16, Milne 11-1933 AB e.mid")
-        self.roll_viewer = RollViewer(self.parent, 1000, 900, self.midi2image.out_img)
+        accel_rate = float(self.accel_rate.get()) / 100 if self.compensate_accel.get() else 0
+        converter = Midi2Image(
+            int(self.roll_dpi.get()),
+            int(self.tempo_slider.get()),
+            accel_rate,
+            float(self.roll_start_pad.get()),
+            float(self.roll_end_pad.get()),
+            float(self.roll_side_margin.get()),
+            float(self.roll_width.get()),
+            float(self.hole_width.get()),
+            float(self.chain_perf_spacing.get()),
+            float(self.single_hole_max_len.get()),
+            float(self.hole_0_center.get()),
+            float(self.hole_99_center.get()),
+        )
+        converter.convert("Hits of the Day 16, Milne 11-1933 AB e.mid")
+        self.roll_viewer = RollViewer(self.parent, 900, 900, converter.out_img)
 
     def create_sidebar(self):
         sidebar = ctk.CTkScrollableFrame(self.parent, corner_radius=0)
-        sidebar.grid(row=0, column=0, sticky="nsew")
+        sidebar.grid(row=0, column=0, padx=(10, 10), sticky="nsew")
         ctk.CTkLabel(sidebar, text="Tracker Bar").grid(row=0, column=0, sticky="w")
-        ctk.CTkOptionMenu(sidebar, values=["88-Note", "Ampico A/B", "Duo-Art"]).grid(row=1, column=0)
+        self.tracker_bar = ctk.CTkOptionMenu(sidebar, values=["88-Note", "Ampico A/B", "Duo-Art"])
+        self.tracker_bar.grid(row=1, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Tempo").grid(row=2, column=0, sticky="w")
-        obj = ctk.CTkSlider(sidebar, from_=30, to=140, number_of_steps=5)
-        obj.set(80)
-        obj.grid(row=3, column=0)
-        
-        obj = ctk.CTkSwitch(sidebar, text="Compensate Acceleration")
-        obj.select()
-        obj.grid(row=4, column=0)
+        self.tempo_label = ctk.CTkLabel(sidebar, text="Tempo:80")
+        self.tempo_label.grid(row=2, column=0, sticky="w")
+        self.tempo_slider = ctk.CTkSlider(sidebar, from_=30, to=140, number_of_steps=(140 - 30) / 5,  # 5 step
+                                          command=lambda val: self.tempo_label.configure(text=f"Tempo:{val:.0f}"))
+        self.tempo_slider.set(80)
+        self.tempo_slider.grid(row=3, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Acceleration rate (%/feet)").grid(row=5, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.2")
-        obj.grid(row=6, column=0)
+        ctk.CTkLabel(sidebar, text="Output Image DPI").grid(row=4, column=0, sticky="w")
+        self.roll_dpi = MyCTkIntInput(sidebar)
+        self.roll_dpi.insert(0, "300")
+        self.roll_dpi.grid(row=5, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Roll width (inch)").grid(row=7, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "11.25")
-        obj.grid(row=8, column=0)
+        ctk.CTkLabel(sidebar, text="Roll width (inch)").grid(row=6, column=0, sticky="w")
+        self.roll_width = MyCTkFloatInput(sidebar)
+        self.roll_width.insert(0, "11.25")
+        self.roll_width.grid(row=7, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Leftest Hole Center from Left Roll Edge (inch)").grid(row=9, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.14")
-        obj.grid(row=10, column=0)
+        ctk.CTkLabel(sidebar, text="Hole 0 center position (inch)").grid(row=8, column=0, sticky="w")
+        self.hole_0_center = MyCTkFloatInput(sidebar)
+        self.hole_0_center.insert(0, "0.14")
+        self.hole_0_center.grid(row=9, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Rightest Hole Center from Left Roll Edge (inch)").grid(row=11, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.14")
-        obj.grid(row=12, column=0)
+        ctk.CTkLabel(sidebar, text="Hole 99 center position (inch)").grid(row=10, column=0, sticky="w")
+        self.hole_99_center = MyCTkFloatInput(sidebar)
+        self.hole_99_center.insert(0, "11.11")
+        self.hole_99_center.grid(row=11, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Padding on roll start (inch)").grid(row=13, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "2.0")
-        obj.grid(row=14, column=0)
+        ctk.CTkLabel(sidebar, text="Padding on roll start (inch)").grid(row=12, column=0, sticky="w")
+        self.roll_start_pad = MyCTkFloatInput(sidebar)
+        self.roll_start_pad.insert(0, "2.0")
+        self.roll_start_pad.grid(row=13, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Padding on roll end (inch)").grid(row=15, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "2.0")
-        obj.grid(row=16, column=0)
+        ctk.CTkLabel(sidebar, text="Padding on roll end (inch)").grid(row=14, column=0, sticky="w")
+        self.roll_end_pad = MyCTkFloatInput(sidebar)
+        self.roll_end_pad.insert(0, "2.0")
+        self.roll_end_pad.grid(row=15, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Margins on roll sides (inch)").grid(row=17, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.25")
-        obj.grid(row=18, column=0)
+        ctk.CTkLabel(sidebar, text="Margins on roll sides (inch)").grid(row=16, column=0, sticky="w")
+        self.roll_side_margin = MyCTkFloatInput(sidebar)
+        self.roll_side_margin.insert(0, "0.25")
+        self.roll_side_margin.grid(row=17, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Hole width (inch)").grid(row=19, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.07")
-        obj.grid(row=20, column=0)
+        ctk.CTkLabel(sidebar, text="Hole width (inch)").grid(row=18, column=0, sticky="w")
+        self.hole_width = MyCTkFloatInput(sidebar)
+        self.hole_width.insert(0, "0.07")
+        self.hole_width.grid(row=19, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Chain Perforation Gap (inch)").grid(row=21, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.0035")
-        obj.grid(row=22, column=0)
+        ctk.CTkLabel(sidebar, text="Single hole max length (inch)").grid(row=20, column=0, sticky="w")
+        self.single_hole_max_len = MyCTkFloatInput(sidebar)
+        self.single_hole_max_len.insert(0, "0.4")
+        self.single_hole_max_len.grid(row=21, column=0, sticky="w")
 
-        ctk.CTkLabel(sidebar, text="Chain Perforation Gap (inch)").grid(row=23, column=0, sticky="w")
-        obj = MyCTkFloatInput(sidebar)
-        obj.insert(0, "0.0035")
-        obj.grid(row=24, column=0)
+        ctk.CTkLabel(sidebar, text="Chain hole spacing (inch)").grid(row=22, column=0, sticky="w")
+        self.chain_perf_spacing = MyCTkFloatInput(sidebar)
+        self.chain_perf_spacing.insert(0, "0.035")
+        self.chain_perf_spacing.grid(row=23, column=0, sticky="w")
+
+        ctk.CTkLabel(sidebar, text="Shorten hole length (px)").grid(row=24, column=0, sticky="w")
+        self.shorten_len = MyCTkIntInput(sidebar)
+        self.shorten_len.insert(0, "10")
+        self.shorten_len.grid(row=25, column=0, sticky="w")
+
+        self.compensate_accel = ctk.CTkSwitch(sidebar, text="Compensate Acceleration")
+        self.compensate_accel.select()
+        self.compensate_accel.grid(row=26, column=0, sticky="w")
+
+        self.acceleration_rate_label = ctk.CTkLabel(sidebar, text="Roll acceleration rate (%/feet)")
+        self.acceleration_rate_label.grid(row=27, column=0, padx=15, sticky="w")
+        self.accel_rate = MyCTkFloatInput(sidebar)
+        self.accel_rate.insert(0, "0.2")
+        self.accel_rate.grid(row=28, column=0, padx=15, sticky="w")
 
         cnv_btn = ctk.CTkButton(sidebar, text="Convert", command=self.convert)
-        cnv_btn.grid(row=25, column=0, sticky="w")
+        cnv_btn.grid(row=29, column=0, sticky="w")
 
 if __name__ == "__main__":
     app = ctk.CTk()
     app.title("PlaySK Midi to Roll Image Converter")
     app.geometry("1400x900")
     app.iconbitmap("PlaySK_icon.ico")
-    app.grid_columnconfigure(0, weight=1)  # Sidebar
+    app.grid_columnconfigure(0, weight=0)  # Sidebar
     app.grid_columnconfigure(1, weight=10)
     app.grid_rowconfigure(0, weight=1)
 
--- src/midi_to_image.py
+++ src/midi_to_image.py
@@ -6,25 +6,38 @@ from PIL import Image, ImageDraw
 
 
 class Midi2Image:
-    def __init__(self):
-        self.roll_dpi = 300
-        self.roll_tempo = 95
-        # Compensate roll acceleration
-        self.roll_accelerate_rate_ft = 0.002  # 0.2% per feet from Stanford University paper. set 0% for no acceleration compensation.
+    def __init__(
+        self,
+        roll_dpi: int = 300,
+        roll_tempo: int = 95,
+        roll_accelerate_rate_ft: float = 0.002,  # 0.2% per feet from Stanford Univ paper. set 0% without acceleration.
+        roll_start_pad: float = 2,
+        roll_end_pad: float = 2,
+        roll_margin: float = 0.25,
+        roll_width: float = 11.25,
+        hole_width: float = 0.07,
+        chain_hole_spacing: float = 0.035,
+        single_hole_max_len: float = 0.4,
+        hole_0_center: float = 0.14,  # from left edge of the roll
+        hole_99_center: float = 11.11,  # from left edge of the roll
+        shorten_hole_px: int = 10
+    ):
+        self.roll_dpi = roll_dpi
+        self.roll_tempo = roll_tempo
+        self.roll_accelerate_rate_ft = roll_accelerate_rate_ft
 
         # in inches
-        self.roll_start_pad = 2
-        self.roll_end_pad = 2
-        self.roll_margin = 0.25
-        self.roll_width = 11.25
-        self.hole_width = 0.07
-        self.chain_perforation_gap = 0.035
-        self.no_chain_perforation_len = 0.4  # This may change slightly
-        self.leftest_hole_center = 0.14  # from left edge of the roll
-        self.rightest_hole_center = self.roll_width - 0.14  # from right edge of the roll
+        self.roll_start_pad = roll_start_pad
+        self.roll_end_pad = roll_end_pad
+        self.roll_margin = roll_margin
+        self.roll_width = roll_width
+        self.hole_width = hole_width
+        self.chain_hole_spacing = chain_hole_spacing
+        self.single_hole_max_len = single_hole_max_len
+        self.hole_0_center = hole_0_center
+        self.hole_99_center = hole_99_center
         self.hole_num = 100
-        self.roll_color= 120  # grayscale
-        self.reduce_px = 10
+        self.roll_color = 120  # in grayscale
 
         # in pixels
         self.roll_start_pad_px = int(self.roll_dpi * self.roll_start_pad)
@@ -32,29 +45,30 @@ class Midi2Image:
         self.roll_margin_px = int(self.roll_dpi * self.roll_margin)
         self.roll_width_px = int(self.roll_dpi * self.roll_width)
         self.hole_width_px = int(self.roll_dpi * self.hole_width)
-        self.chain_perforation_gap_px = int(self.roll_dpi * self.chain_perforation_gap)
-        self.no_chain_perforation_len_px  = int(self.roll_dpi * self.no_chain_perforation_len)
+        self.chain_perforation_spacing_px = int(self.roll_dpi * self.chain_hole_spacing)
+        self.single_hole_max_len_px = int(self.roll_dpi * self.single_hole_max_len)
+        self.shorten_hole_px = shorten_hole_px
 
         self.control_change_map = [
             {"controlChangeNo": 64, "midiNoteNo": 18},
-			{"controlChangeNo": 67, "midiNoteNo": 113},	
-		]
+            {"controlChangeNo": 67, "midiNoteNo": 113},
+        ]
         self.hole_x_list = [self._get_hole_x(i) for i in range(128)]
-        self.out_img: Image.Image = None
-        self.draw: ImageDraw = None
+        self.out_img = None
+        self.draw = None
 
     def get_roll_acceleration_rate(self, px: int) -> float:
         cur_feet = px / self.roll_dpi / 12.0
         return (1 + self.roll_accelerate_rate_ft) ** cur_feet
 
     def _get_hole_x(self, note_no):
-        return int(self.roll_dpi * (self.roll_margin + self.leftest_hole_center + ((note_no) * (self.rightest_hole_center - self.leftest_hole_center) / (self.hole_num - 1)) - (self.hole_width / 2)))
+        return int(self.roll_dpi * (self.roll_margin + self.hole_0_center + ((note_no) * (self.hole_99_center - self.hole_0_center) / (self.hole_num - 1)) - (self.hole_width / 2)))
 
     def get_tick_to_px(self, tick_len, tempo, bpm, ppq):
         return int(((tick_len * self.roll_dpi * tempo * 1.2) / (bpm * ppq)))
 
     def draw_hole(self, note_no, tempo, bpm, ppq, on_tick, off_tick):
-        hole_h = self.get_tick_to_px(off_tick - on_tick, tempo, bpm, ppq) - self.reduce_px
+        hole_h = self.get_tick_to_px(off_tick - on_tick, tempo, bpm, ppq) - self.shorten_hole_px
         hole_h = max(hole_h, self.hole_width_px)
         hole_x = self.hole_x_list[note_no - 15]
         hole_y1 = self.get_tick_to_px(on_tick, tempo, bpm, ppq) + self.roll_start_pad_px
@@ -68,11 +82,11 @@ class Midi2Image:
         hole_y1 = self.out_img.height - hole_y1
         hole_y2 = self.out_img.height - hole_y2
 
-		# Chain Perforation
+        # Chain Perforation
         y = hole_y2
-        while y < hole_y1 - self.no_chain_perforation_len_px:
+        while y < hole_y1 - self.single_hole_max_len_px:
             self.draw.ellipse([hole_x, y, hole_x + self.hole_width_px, y + self.hole_width_px], fill=255)
-            y += self.chain_perforation_gap_px + self.hole_width_px
+            y += self.chain_perforation_spacing_px + self.hole_width_px
 
         # Normal perforation
         self.draw.rounded_rectangle([hole_x, y, hole_x + self.hole_width_px, hole_y1], radius=self.hole_width_px // 2, fill=255)
@@ -85,7 +99,7 @@ class Midi2Image:
         note_on_ticks = [0] * 128
         initialized = False
         total_ticks = 0
-        for track in mid.tracks: 
+        for track in mid.tracks:
             total_ticks = max(sum([t.time for t in track]), total_ticks)
 
         for track in mid.tracks:
@@ -121,9 +135,9 @@ class Midi2Image:
                     elif msg.type == "note_off" or (msg.type == "note_on" and msg.velocity == 0):
                         self.draw_hole(msg.note, self.roll_tempo, bpm, ppq, note_on_ticks[msg.note], abs_tick)
 
-
     def saveimg(self, savepath: str) -> None:
         self.out_img.save(savepath)
+
 
 if __name__ == "__main__":
     input_dir = "C:\\Users\\sasaki\\Downloads\\Ampico_All_erolls\\test\\"
