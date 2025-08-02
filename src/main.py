4dadd70e00c5566fd9b89f022d3eae918c965473
nai-kon <fxtch686@yahoo.co.jp>
2025-08-02 16:32:27 +0900

    update image automatically when on_enter/leave the control

--- src/main.py
+++ src/main.py
@@ -57,7 +57,6 @@ class MainFrame():
         ctk.set_appearance_mode("Dark" if self.conf.dark_mode else "Light")
 
     def convert(self) -> None:
-        print("convert called")
         if self.midi_file_path is None:
             return
 
@@ -124,6 +123,9 @@ class MainFrame():
         ctk.CTkLabel(msgbox, text=f"Length: {length:.2f} feet @{dpi}DPI", font=font).pack(padx=10, pady=5, anchor="w")
 
     def create_sidebar(self):
+        def on_change(value=None) -> None:
+            self.convert()  # update image when option menu changes
+            
         sidebar = CustomScrollableFrame(self.parent, corner_radius=0, fg_color=("#CCCCCC", "#111111"))
         # sidebar = ctk.CTkFrame(self.parent, corner_radius=0, fg_color=("#CCCCCC", "#111111"))
         sidebar.grid(row=0, column=0, sticky="nsew")
@@ -133,34 +135,40 @@ class MainFrame():
         self.fileopen.pack(padx=10, pady=(10, 0), anchor="w", fill="both")
 
         ctk.CTkLabel(sidebar, text="Tracker Bar").pack(padx=10, anchor="w")
-        self.tracker_bar = ctk.CTkOptionMenu(sidebar, values=["88-Note", "AmpicoA"], command=lambda e: self.convert())
+        self.tracker_bar = ctk.CTkOptionMenu(sidebar, values=["88-Note", "AmpicoA"], command=on_change)
         self.tracker_bar.set(self.conf.tracker)
         self.tracker_bar.pack(padx=10, anchor="w", fill="both")
 
         self.tempo_label = ctk.CTkLabel(sidebar, text=f"Tempo:{self.conf.tempo}")
         self.tempo_label.pack(padx=10, anchor="w")
+        self.prev_tempo = -1
+        def tempo_change(tempo):
+            if tempo != self.prev_tempo:
+                self.tempo_label.configure(text=f"Tempo:{tempo:.0f}")
+                on_change()
+            self.prev_tempo = tempo
         self.tempo_slider = ctk.CTkSlider(sidebar, from_=30, to=140, number_of_steps=(140 - 30) / 5,  # interval of 5
-                                          command=lambda val: self.tempo_label.configure(text=f"Tempo:{val:.0f}"))
+                                          command=tempo_change)
         self.tempo_slider.set(self.conf.tempo)
         self.tempo_slider.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Output Image DPI").pack(padx=10, anchor="w")
-        self.roll_dpi = MyCTkIntInput(sidebar)
+        self.roll_dpi = MyCTkIntInput(sidebar, on_change)
         self.roll_dpi.insert(0, self.conf.dpi)
         self.roll_dpi.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Roll width (inch)").pack(padx=10, anchor="w")
-        self.roll_width = MyCTkFloatInput(sidebar)
+        self.roll_width = MyCTkFloatInput(sidebar, on_change)
         self.roll_width.insert(0, self.conf.roll_width)
         self.roll_width.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Hole 0 center position (inch)").pack(padx=10, anchor="w")
-        self.hole_0_center = MyCTkFloatInput(sidebar)
+        self.hole_0_center = MyCTkFloatInput(sidebar, on_change)
         self.hole_0_center.insert(0, self.conf.hole_0_center)
         self.hole_0_center.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Hole 99 center position (inch)").pack(padx=10, anchor="w")
-        self.hole_99_center = MyCTkFloatInput(sidebar)
+        self.hole_99_center = MyCTkFloatInput(sidebar, on_change)
         self.hole_99_center.insert(0, self.conf.hole_99_center)
         self.hole_99_center.pack(padx=10, anchor="w")
 
@@ -175,38 +183,38 @@ class MainFrame():
         # self.roll_end_pad.grid(row=16, column=0, padx=10, sticky="w")
 
         ctk.CTkLabel(sidebar, text="Margins on roll sides (inch)").pack(padx=10, anchor="w")
-        self.roll_side_margin = MyCTkFloatInput(sidebar)
+        self.roll_side_margin = MyCTkFloatInput(sidebar, on_change)
         self.roll_side_margin.insert(0, self.conf.roll_side_margin)
         self.roll_side_margin.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Hole width (inch)").pack(padx=10, anchor="w")
-        self.hole_width = MyCTkFloatInput(sidebar)
+        self.hole_width = MyCTkFloatInput(sidebar, on_change)
         self.hole_width.insert(0, self.conf.hole_width)
         self.hole_width.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Single hole max length (inch)").pack(padx=10, anchor="w")
-        self.single_hole_max_len = MyCTkFloatInput(sidebar)
+        self.single_hole_max_len = MyCTkFloatInput(sidebar, on_change)
         self.single_hole_max_len.insert(0, self.conf.single_hole_max_len)
         self.single_hole_max_len.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Chain hole spacing (inch)").pack(padx=10, anchor="w")
-        self.chain_perf_spacing = MyCTkFloatInput(sidebar)
+        self.chain_perf_spacing = MyCTkFloatInput(sidebar, on_change)
         self.chain_perf_spacing.insert(0, self.conf.chain_perf_spacing)
         self.chain_perf_spacing.pack(padx=10, anchor="w")
 
         ctk.CTkLabel(sidebar, text="Shorten hole length (px)").pack(padx=10, anchor="w")
-        self.shorten_len = MyCTkIntInput(sidebar)
+        self.shorten_len = MyCTkIntInput(sidebar, on_change)
         self.shorten_len.insert(0, self.conf.shorten_len)
         self.shorten_len.pack(padx=10, anchor="w")
 
-        self.compensate_accel = ctk.CTkSwitch(sidebar, text="Compensate Acceleration")
+        self.compensate_accel = ctk.CTkSwitch(sidebar, text="Compensate Acceleration", command=on_change)
         if self.conf.compensate_accel:
             self.compensate_accel.select()
         self.compensate_accel.pack(padx=10, pady=(10, 0), anchor="w")
 
         self.acceleration_rate_label = ctk.CTkLabel(sidebar, text="Acceleration rate (%/feet)")
         self.acceleration_rate_label.pack(padx=25, anchor="w")
-        self.accel_rate = MyCTkFloatInput(sidebar)
+        self.accel_rate = MyCTkFloatInput(sidebar, on_change)
         self.accel_rate.insert(0, self.conf.accel_rate)
         self.accel_rate.pack(padx=25, anchor="w")
 
