fdad6fd6a287e95598e0164085801ee24ec82ee8
nai-kon <fxtch686@yahoo.co.jp>
2025-08-09 09:23:21 +0900

    update Ampico A conversion

--- src/converter_ampico_a.py
+++ src/converter_ampico_a.py
@@ -3,11 +3,15 @@ from converter_base import BaseConverter
 
 
 class AmpicoA(BaseConverter):
-    """Ampico A for MIDI to Image conversion."""    
     def __init__(self, conf: ConfigMng) -> None:
         super().__init__(conf)
+
+        # note length of long shaped holes need to be shorten
+        normal_hole_h = 0.0625 * self.roll_dpi  # px
+        crescendo_hole_h = 0.34 * self.roll_dpi  # px
+        half_offset_h = (crescendo_hole_h - normal_hole_h) / 2
         self.custom_hole_offsets = {
-            16: {"top_offset": 41, "bottom_offset": -41},  # Duo-Art specific offsets
-            111: {"top_offset": 41, "bottom_offset": -41}, # Ampico Soft Pedal
-            113: {"top_offset": 41, "bottom_offset": -41}, # Ampico Treble Crescendo
+            16: {"top_offset": half_offset_h, "bottom_offset": -half_offset_h},  # bass crescendo
+            111: {"top_offset": half_offset_h, "bottom_offset": -half_offset_h}, # soft pedal
+            113: {"top_offset": half_offset_h, "bottom_offset": -half_offset_h}, # treble crescendo
         }
