c802b645567abf8f4ec829b61b48e64dceef03f6
nai-kon <fxtch686@yahoo.co.jp>
2025-10-12 01:06:19 +0900

    WIP add Aeolian 176-note

--- src/converter_duoart_organ.py
+++ src/converter_duoart_organ.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 
 from config import ConfigMng
 from converter_base import BaseConverter
-from custom_widgets import CustomScrollableFrame
+from custom_widgets import CustomScrollableFrame, MyCTkIntInput
 
 
 class DuoArtOrgan(BaseConverter):
@@ -60,103 +60,96 @@ class DuoArtOrgan(BaseConverter):
         self.draw: ImageDraw | None = None
 
 
-# class DuoArtOrganSetting:
-#     def __init__(self, parent: ctk.CTk):
-#         self.dialog = CustomScrollableFrame(parent, fg_color=("#CCCCCC", "#111111"))
-#         self.dialog.grid(row=0, column=0, sticky="nsew")    
-
-#         # self.midi_settings = {
-#         #     # hole_no: [name, midi_ch, midi_note_no]
-#         #     # upper rows
-#         #     1: 
-#         # }
-        
-    
-#         # MIDI setting for upper row holes
-
-#         ctk.CTkLabel(self.dialog, text="Upper row holes (Swell)").pack(padx=10, anchor="w")
-
-
 class DuoArtOrganSetting:
     def __init__(self, parent: ctk.CTk):
         self.dialog = CustomScrollableFrame(parent, fg_color=("#CCCCCC", "#111111"))
         self.dialog.grid(row=0, column=0, sticky="nsew")    
 
-        self.midi_settings = {
-            #"Hole Number", "Name", "MIDI Channel", "MIDI Note Number", midi_ch_obj, midi_noteno_obj]
+        self.midi_settings: dict[str, dict[str, int | dict[int, list]]] = {
+            #"Hole Number", "Name", "MIDI Note Number", midi_noteno_obj]
             "Lower playing notes (Great)":{
-                32: ["Great Lowest Note", 0, 0, None, None],
-                146: ["Great Highest Note", 0, 0, None, None],
+                "Midi Channel": 0,
+                "Holes": {
+                    32: ["Great Lowest Note", 0, None],
+                    146: ["Great Highest Note", 0, None],
+                }
             },
             "Lower control holes (Great)": {
-                # lower holes
-                0: ["Great Tremolo", 0, 0, None, None],
-                2: ["Great Tonal", 0, 0, None, None],
-                4: ["Great Harp", 0, 0, None, None],
-                6: ["Great Extension", 0, 0, None, None],
-                8: ["Pedal 2nd Octave", 0, 0, None, None],
-                10: ["Pedal 3rd Octave", 0, 0, None, None],
-                12: ["Great Shade#1", 0, 0, None, None],
-                14: ["Great Shade#2", 0, 0, None, None],
-                16: ["Great Shade#3", 0, 0, None, None],
-                18: ["Great Shade#4", 0, 0, None, None],
-                20: ["Great Shade#5", 0, 0, None, None],
-                22: ["Great Shade#6", 0, 0, None, None],
-                24: ["Pedal Bassoon 16'", 0, 0, None, None],
-                26: ["Pedal String 16'", 0, 0, None, None],
-                28: ["Pedal Flute F 16'", 0, 0, None, None],
-                30: ["Pedal Flute P 16'", 0, 0, None, None],
-                148: ["Great String PP", 0, 0, None, None],
-                150: ["Great String P", 0, 0, None, None],
-                152: ["Great String F", 0, 0, None, None],
-                154: ["Great Flute P", 0, 0, None, None],
-                156: ["Great Flute F", 0, 0, None, None],
-                158: ["Great Flute 4'", 0, 0, None, None],
-                160: ["Great Diapason F", 0, 0, None, None],
-                162: ["Great Piccolo", 0, 0, None, None],
-                164: ["Great Clarinet", 0, 0, None, None],
-                166: ["Great Trumpet", 0, 0, None, None],
-                168: ["Chimes Dampers Off", 0, 0, None, None],
-                170: ["L.C.W", 0, 0, None, None],
-                172: ["L.C.W.", 0, 0, None, None],
-                174: ["Ventil Control", 0, 0, None, None],
+                "Midi Channel": 4,
+                "Holes": {
+                    0: ["Great Tremolo", 0, None],
+                    2: ["Great Tonal", 0, None],
+                    4: ["Great Harp", 0, None],
+                    6: ["Great Extension", 0, None],
+                    8: ["Pedal 2nd Octave", 0, None],
+                    10: ["Pedal 3rd Octave", 0, None],
+                    12: ["Great Shade#1", 0, None],
+                    14: ["Great Shade#2", 0, None],
+                    16: ["Great Shade#3", 0, None],
+                    18: ["Great Shade#4", 0, None],
+                    20: ["Great Shade#5", 0, None],
+                    22: ["Great Shade#6", 0, None],
+                    24: ["Pedal Bassoon 16'", 0, None],
+                    26: ["Pedal String 16'", 0, None],
+                    28: ["Pedal Flute F 16'", 0, None],
+                    30: ["Pedal Flute P 16'", 0, None],
+                    148: ["Great String PP", 0, None],
+                    150: ["Great String P", 0, None],
+                    152: ["Great String F", 0, None],
+                    154: ["Great Flute P", 0, None],
+                    156: ["Great Flute F", 0, None],
+                    158: ["Great Flute 4'", 0, None],
+                    160: ["Great Diapason F", 0, None],
+                    162: ["Great Piccolo", 0, None],
+                    164: ["Great Clarinet", 0, None],
+                    166: ["Great Trumpet", 0, None],
+                    168: ["Chimes Dampers Off", 0, None],
+                    170: ["L.C.W", 0, None],
+                    172: ["L.C.W.", 0, None],
+                    174: ["Ventil Control", 0, None]
+                }
             },
             "Upper playing notes (Swell)":{
-                33: ["Swell Lowest Note", 0, 0, None, None],
-                147: ["Swell Highest Note", 0, 0, None, None],
+                "Midi Channel": 1,
+                "Holes": {
+                    33: ["Swell Lowest Note", 0, None],
+                    147: ["Swell Highest Note", 0, None],
+                }
             },
             "Upper control holes (Swell)":{
-
-                1: ["Swell Echo", 0, 0, None, None],
-                3: ["Swell Chimes", 0, 0, None, None],
-                5: ["Swell Tremolo", 0, 0, None, None],
-                7: ["Swell Harp", 0, 0, None, None],
-                9: ["Swell Trumpet", 0, 0, None, None],
-                11: ["Swell Oboe", 0, 0, None, None],
-                13: ["Swell Vox Humana", 0, 0, None, None],
-                15: ["Swell Diapason MF", 0, 0, None, None],
-                17: ["Swell Flute 16'", 0, 0, None, None],
-                19: ["Swell Flute 4'", 0, 0, None, None],
-                21: ["Swell Flute P", 0, 0, None, None],
-                23: ["Swell String Vibrato F", 0, 0, None, None],
-                25: ["Swell String F", 0, 0, None, None],
-                27: ["Swell String MF", 0, 0, None, None],
-                29: ["Swell String P", 0, 0, None, None],
-                31: ["Swell String PP", 0, 0, None, None],
-                149: ["Swell Shade#1", 0, 0, None, None],
-                151: ["Swell Shade#2", 0, 0, None, None],
-                153: ["Swell Shade#3", 0, 0, None, None],
-                155: ["Swell Shade#4", 0, 0, None, None],
-                157: ["Swell Shade#5", 0, 0, None, None],
-                159: ["Swell Shade#6", 0, 0, None, None],
-                161: ["Swell Extension", 0, 0, None, None],
-                163: ["L.C.W.", 0, 0, None, None],
-                165: ["L.C.W.", 0, 0, None, None],
-                167: ["Swell Soft Chimes", 0, 0, None, None],
-                169: ["Reroll", 0, 0, None, None],
-                171: ["Ventil Control", 0, 0, None, None],
-                173: ["Normal", 0, 0, None, None],
-                175: ["Pedal to Upper Holes", 0, 0, None, None],
+                "Midi Channel": 5,
+                "Holes": {
+                    1: ["Swell Echo", 0, None],
+                    3: ["Swell Chimes", 0, None],
+                    5: ["Swell Tremolo", 0, None],
+                    7: ["Swell Harp", 0, None],
+                    9: ["Swell Trumpet", 0, None],
+                    11: ["Swell Oboe", 0, None],
+                    13: ["Swell Vox Humana", 0, None],
+                    15: ["Swell Diapason MF", 0, None],
+                    17: ["Swell Flute 16'", 0, None],
+                    19: ["Swell Flute 4'", 0, None],
+                    21: ["Swell Flute P", 0, None],
+                    23: ["Swell String Vibrato F", 0, None],
+                    25: ["Swell String F", 0, None],
+                    27: ["Swell String MF", 0, None],
+                    29: ["Swell String P", 0, None],
+                    31: ["Swell String PP", 0, None],
+                    149: ["Swell Shade#1", 0, None],
+                    151: ["Swell Shade#2", 0, None],
+                    153: ["Swell Shade#3", 0, None],
+                    155: ["Swell Shade#4", 0, None],
+                    157: ["Swell Shade#5", 0, None],
+                    159: ["Swell Shade#6", 0, None],
+                    161: ["Swell Extension", 0, None],
+                    163: ["L.C.W.", 0, None],
+                    165: ["L.C.W.", 0, None],
+                    167: ["Swell Soft Chimes", 0, None],
+                    169: ["Reroll", 0, None],
+                    171: ["Ventil Control", 0, None],
+                    173: ["Normal", 0, None],
+                    175: ["Pedal to Upper Holes", 0, None],
+                }
             },
         }
 
@@ -165,23 +158,31 @@ class DuoArtOrganSetting:
         for key, val in self.midi_settings.items():
             if key == "Upper playing notes (Swell)":
                 row_no = 0
-                col_no = 4
+                col_no = 3
 
             # label
             font = ctk.CTkFont(size=20)
             section = ctk.CTkLabel(self.dialog, text=key, font=font)
-            section.grid(row=row_no, column=col_no, columnspan=4, padx=5, pady=(20, 10))
+            section.grid(row=row_no, column=col_no, columnspan=3, padx=5, pady=(30, 10))
+            row_no += 1
+
+            # MIDI Channel
+            ctk.CTkLabel(self.dialog, text="Midi Ch").grid(row=row_no, column=col_no, padx=5, pady=5)
+            tmp = ctk.CTkComboBox(self.dialog, values=[str(i) for i in range(16)], width=80)
+            tmp.set(val["Midi Channel"])
+            tmp.grid(row=row_no, column=col_no + 1, padx=5, pady=5)
+            self.midi_settings[key]["Midi Channel"] = tmp
             row_no += 1
 
             # header
-            headers = ["Hole No", "Name", "MIDI CH", "MIDI Note No"]
+            headers = ["Hole No", "Name", "MIDI Note No"]
             for i, text in enumerate(headers):
                 label = ctk.CTkLabel(self.dialog, text=text)
                 label.grid(row=row_no, column=col_no + i, padx=5, pady=5)
             row_no += 1
 
-            for hole_no, (hole_name, midi_ch, midi_no, _, _) in val.items():    
-                # Hole No.
+            for hole_no, (hole_name, midi_no, _) in val["Holes"].items():
+                # Hole No.z
                 hole_label = ctk.CTkLabel(self.dialog, text=str(hole_no))
                 hole_label.grid(row=row_no, column=col_no, padx=5, pady=2)
 
@@ -189,17 +190,11 @@ class DuoArtOrganSetting:
                 name_entry = ctk.CTkLabel(self.dialog, text=hole_name)
                 name_entry.grid(row=row_no, column=col_no + 1, padx=5, pady=2)
 
-                # MIDI Channel
-                tmp = ctk.CTkComboBox(self.dialog, values=[str(i) for i in range(16)], width=80)
-                tmp.set(str(midi_ch))
-                tmp.grid(row=row_no, column=col_no + 2, padx=5, pady=2)
-                self.midi_settings[key][hole_no][3] = tmp
-
                 # Note Number
-                tmp = ctk.CTkComboBox(self.dialog, values=[str(i) for i in range(128)], width=80)
-                tmp.set(str(midi_no))
-                tmp.grid(row=row_no, column=col_no + 3, padx=5, pady=2)
-                self.midi_settings[key][hole_no][4] = tmp
+                tmp = MyCTkIntInput(self.dialog, width=50)
+                tmp.insert(0, midi_no)
+                tmp.grid(row=row_no, column=col_no + 2, padx=5, pady=2)
+                self.midi_settings[key]["Holes"][hole_no][2] = tmp
 
                 row_no += 1
 
@@ -209,13 +204,13 @@ class DuoArtOrganSetting:
     def debug(self):
         # debug
         for row_pos, val in self.midi_settings.items():
-            for hole_no, [hole_name, midi_ch, midi_no, midi_ch_obj, midi_noteno_obj] in val.items():
-                print(hole_name, midi_ch_obj.get(), midi_noteno_obj.get())
+            for hole_no, (hole_name, midi_no, midi_noteno_obj) in val["Holes"].items():
+                print(hole_name, midi_noteno_obj.get())
 
 
 if __name__ == "__main__":
     app = ctk.CTk()
-    app.geometry("800x600")
+    app.geometry("650x600")
 
     app.grid_columnconfigure(0, weight=1)  #
     app.grid_rowconfigure(0, weight=1)
--- src/custom_widgets.py
+++ src/custom_widgets.py
@@ -6,11 +6,11 @@ from tkinterdnd2 import TkinterDnD
 
 
 class MyCTkFloatInput(ctk.CTkEntry):
-    def __init__(self, parent, on_change: Callable[[Any], None] | None = None) -> None:
+    def __init__(self, parent, on_change: Callable[[Any], None] | None = None, **kwargs) -> None:
         self.prev_value: str | None = None
         self.on_change = on_change
         validate_cmd = parent.register(self.validate_input)
-        super().__init__(parent, validate="key", validatecommand=(validate_cmd, "%P"))
+        super().__init__(parent, validate="key", validatecommand=(validate_cmd, "%P"), **kwargs)
         self.bind("<Return>", command=self.on_change_inner)  # callback on Enter
         self.bind("<FocusOut>", command=self.on_change_inner)  # callback on leave input
 
